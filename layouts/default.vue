3c59270cec31aac5de78557a56a0b2801d1f3996
yevhenii <testjokerqwerty@gmail.com>
2024-08-23 10:40:59 +0300

    added default Layout wrapper

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineComponent({
+	name: 'LayoutDefault',
+});
+</script>
+
+<template>
+	<main class="mx-auto max-w-4xl">
+		<!-- header -->
+		<slot />
+		<!-- footer -->
+	</main>
+</template>
